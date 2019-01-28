a78d1d0b2f0a38e6b5261a9b4e53f13892a4cf08
Anthony <42888128+BunBunXXi@users.noreply.github.com>
2019-01-27 18:32:34 -0800

    Organization

--- Solution/README.txt
+++
@@ -1,4 +0,0 @@
-Solution to the projects of 
-https://github.com/jorgegonzalez/beginner-projects
-
-Will add more later
--- WordPyramid/Word.py
+++
@@ -1,8 +0,0 @@
-phrase = input("Enter your phrase: ")
-
-for x in range(len(phrase)):
-    if phrase[x] != ' ':
-        print(phrase[0:x])
-for x in range(len(phrase)):
-    if phrase[len(phrase)-1-x] != ' ':
-        print(phrase[0:len(phrase)-x])
